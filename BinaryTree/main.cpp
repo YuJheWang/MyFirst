1e34ed78a2d4aca377ca10c564fde8d60b2ea9b5
YuJheWang <yuzhewang0722@gmail.com>
2023-02-17 23:28:30 +0800

    Add BinaryTree

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -4,15 +4,25 @@
 #include "BinaryTree.hpp"
 
 template <typename T, typename Iter>
-void sort(Iter begin, Iter end)
+std::vector<T> sort(Iter begin, Iter end)
 {
+    std::vector<T> result;
     BinaryTreeNode<T>* root  = Read<T>(begin, end);
+    for (int i = 0; i < end - begin; i++)
+    {
+        BinaryTreeNode<T>* leftest = root->GetLeftest();
+        result.push_back(leftest->value);
+        leftest->Delete();
+    }
 }
 
 int main()
 {
     std::vector<float> number = {1.0f, 5.2f, 3.1f, 8.5f, 8.9f, 5.1f};
-    BinaryTreeNode<float>* root = Read<float>(number.begin(), number.end());
+    std::vector<float> sorted = sort<float>(number.begin(), number.end());
 
-    delete root;
+    for (auto i : sorted)
+        std::cout << i << " " << std::endl;
+
+    return 0;
 }

9f5b79bac0e834242f9447b980d69a7e5959dbbb
YuJheWang <yuzhewang0722@gmail.com>
2023-02-17 22:45:16 +0800

    Add BinaryTree

---
+++ BinaryTree/BinaryTree.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+template <typename T>
+class BinaryTreeNode {
+public:
+
+    T value;
+
+    BinaryTreeNode* left{nullptr}, right{nullptr}, parent{nullptr};
+
+    BinaryTreeNode(T v) : value(v) {}
+
+    ~BinaryTreeNode() { delete left; delete right; }
+
+    void Add(BinaryTreeNode* node);
+
+    void Delete();
+
+private:
+};
